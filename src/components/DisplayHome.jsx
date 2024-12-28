ef5a2c8abcef3b1b39982fafd137a711744bb5fb
NithinJoelJ <NithinJoelJ>
2024-12-29 02:09:37 +0530

    Spotify-Clone

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import Albumitem from './Albumitem'
+import { songsData } from '../assets/assets'
+import Songitem from './Songitem'
+
+const DisplayHome = () => {
+  return (
+    <>
+      <Navbar />
+      <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+        <div className='flex overflow-auto '>
+        {albumsData.map((items,index)=>(<Albumitem key={index} name={items.name} desc={items.desc} id={items.id} image={items.image}/>))}
+        </div>
+      </div>
+      <div className='mb-4'>
+        <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
+        <div className='flex overflow-auto '>
+        {songsData.map((items,index)=>(<Songitem key={index} name={items.name} desc={items.desc} id={items.id} image={items.image}/>))}
+        </div>
+      </div>
+    </>
+  )
+} 
+
+export default DisplayHome
